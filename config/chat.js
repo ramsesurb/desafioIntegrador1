61efe248d14f96bf28a46a73511522c0624b5d96
ramsesurb <103768498+ramsesurb@users.noreply.github.com>
2023-05-08 16:15:34 -0300

    Update chat.js

--- config/chat.js
+++ config/chat.js
@@ -1,1 +1,23 @@
+import mongoose from "mongoose"
 
+const Collection = 'Products'
+
+const chatSchema = new mongoose.Schema({
+  id: {
+    type: Number,
+    unique: true,
+  },
+  usuario: {
+    type: String,
+    required: true,
+  },
+  mensaje: {
+    type: String,
+    required: true,
+  }
+});
+  
+const productoModel = mongoose.model(Collection, chatSchema);
+ 
+
+  export default productoModel
